46f6962bc22e9f89d11c8756f45a4b9e4549a01d
rabriol <rafaeuoliveira@gmail.com>
2016-04-05 00:35:15 -0300

    updating configuration

--- src/main/java/com/ibm/controller/WebController.java
+++ src/main/java/com/ibm/controller/WebController.java
@@ -2,27 +2,26 @@ package com.ibm.controller;
 
 import com.ibm.logic.PeopleExtractor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by brito on 4/3/16.
  */
-@Controller
+@RestController
 public class WebController {
 
     @Autowired
     private PeopleExtractor peopleExtractor;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 
     @RequestMapping(value = "/find", method = RequestMethod.GET)
-    public String find(Model model) {
+    public String find() {
 
 
 
